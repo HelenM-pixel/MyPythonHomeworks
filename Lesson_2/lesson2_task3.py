79aa480b0f304cbf154949591e259152db458836
Helen <helen@td>
2025-10-06 14:42:58 +0700

    Исправлен синтаксис

--- Lesson_2/lesson2_task3.py
+++ Lesson_2/lesson2_task3.py
@@ -4,6 +4,7 @@ def square(a):
 
 
 side = float(input("Введите сторону квадрата: "))
-if side != int(side): side = int(side) + 1
+if side != int(side):
+    side = int(side) + 1
 answer = square(side)
 print(f"Площадь квадрата со стороной {side} равна {answer}")
--- Lesson_2/lesson2_task4.py
+++ Lesson_2/lesson2_task4.py
@@ -1,10 +1,10 @@
-def fizz_buzz(n):
-    for x in range(1, n + 1):
+def fizz_buzz(num):
+    for x in range(1, num + 1):
         if x % 5 == 0 and x % 3 == 0:
             print("fizzbuzz")
         elif x % 3 == 0:
             print("fizz")
-        elif x %  5 == 0:
+        elif x % 5 == 0:
             print("buzz")
 
         else:
