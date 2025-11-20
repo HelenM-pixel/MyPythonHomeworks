69cb3daca49434aa55e8a6b0d98c2e744ca8f4d3
Helen <helen@td>
2025-11-20 19:10:05 +0700

    Исправленная ветка контрольной урока 6

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -4,154 +4,37 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
 
 
-driver = webdriver.Edge(
-    service=EdgeService(EdgeChromiumDriverManager().install()))
-driver.get(
+def test_01():
+    driver = webdriver.Edge(
+        service=EdgeService(EdgeChromiumDriverManager().install()))
+    driver.get(
     "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-
-
-# нашли и очистили поле ввода, послали новое значение
-first_name = driver.find_element(
-    By.CSS_SELECTOR, ".form-control")
-first_name.clear()
-first_name.send_keys("Иван")
-
-
-last_name = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[1]/div[2]/label/input")
-last_name.clear()
-last_name.send_keys("Петров")
-
-
-address = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[2]/div[1]/label/input")
-address.clear()
-address.send_keys("Ленина, 55-3")
-
-
-email = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[3]/div[1]/label/input")
-email.clear()
-email.send_keys("test@skypro.com")
-
-
-phone_number = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[3]/div[2]/label/input")
-phone_number.clear()
-phone_number.send_keys("+7985899998787")
-
-
-zip_code = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[2]/div[2]/label/input")
-zip_code.clear()
-zip_code.send_keys("")
-
-
-city = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[2]/div[3]/label/input")
-city.clear()
-city.send_keys("Москва")
-
-
-country = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[2]/div[4]/label/input")
-country.clear()
-country.send_keys("Россия")
-
-
-job_position = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[4]/div[1]/label/input")
-job_position.clear()
-job_position.send_keys("QA")
-
-
-company = driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[4]/div[2]/label/input")
-company.clear()
-company.send_keys("SkyPro")
-
-
-submit_button = (driver.find_element(
-    By.XPATH, "/html/body/main/div/form/div[5]/div/button"))
-submit_button.click()
-
-
-# проверка assert - что поле zip красное
-colour_field_zip = driver.find_element(
-    By.ID, "zip-code").get_attribute("class")
-print(f"{colour_field_zip}")
-red_zip = f"{colour_field_zip}"
-assert red_zip == "alert py-2 alert-danger"
-
-
-# проверки, что остальные поля зелёные
-colour_field_first_name = driver.find_element(
-    By.ID, "first-name").get_attribute("class")
-print(f"{colour_field_first_name}")
-green_first_name = f"{colour_field_first_name}"
-assert green_first_name == "alert py-2 alert-success"
-
-
-colour_field_last_name = driver.find_element(
-    By.ID, "last-name").get_attribute("class")
-print(f"{colour_field_last_name}")
-green_last_name = f"{colour_field_last_name}"
-assert green_last_name == "alert py-2 alert-success"
-
-
-colour_field_address = driver.find_element(
-    By.ID, "address").get_attribute("class")
-print(f"{colour_field_address}")
-green_address = f"{colour_field_address}"
-assert green_address == "alert py-2 alert-success"
-
-
-colour_field_city = driver.find_element(
-    By.ID, "city").get_attribute("class")
-print(f"{colour_field_city}")
-green_city = f"{colour_field_city}"
-assert green_city == "alert py-2 alert-success"
-
-
-colour_field_country = driver.find_element(
-    By.ID, "country").get_attribute("class")
-print(f"{colour_field_country}")
-green_country = f"{colour_field_country}"
-assert green_country == "alert py-2 alert-success"
-
-
-colour_field_city = driver.find_element(
-    By.ID, "city").get_attribute("class")
-print(f"{colour_field_city}")
-green_city = f"{colour_field_city}"
-assert green_city == "alert py-2 alert-success"
-
-
-colour_field_e_mail = driver.find_element(
-    By.ID, "e-mail").get_attribute("class")
-print(f"{colour_field_e_mail}")
-green_e_mail = f"{colour_field_e_mail}"
-assert green_e_mail == "alert py-2 alert-success"
-
-
-colour_field_phone = driver.find_element(
-    By.ID, "phone").get_attribute("class")
-print(f"{colour_field_phone}")
-green_phone = f"{colour_field_phone}"
-assert green_phone == "alert py-2 alert-success"
-
-
-colour_field_job_position = driver.find_element(
-    By.ID, "job-position").get_attribute("class")
-print(f"{colour_field_job_position}")
-green_job_position = f"{colour_field_job_position}"
-assert green_job_position == "alert py-2 alert-success"
-
-colour_field_company = driver.find_element(
-    By.ID, "company").get_attribute("class")
-print(f"{colour_field_company}")
-green_company = f"{colour_field_company}"
-assert green_company == "alert py-2 alert-success"
-
-
-driver.quit()
+    # нашли поле ввода, послали новое значение
+    driver.find_element(By.CSS_SELECTOR, ".form-control").clear()
+    driver.find_element(By.CSS_SELECTOR, ".form-control").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "zip-code").send_keys("")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+    driver.find_element(
+        By.CSS_SELECTOR, "button[type='submit']").click()
+    # проверка assert - что поле zip красное
+    colour_field_zip = driver.find_element(
+        By.ID, "zip-code").get_attribute("class")
+    red_zip = f"{colour_field_zip}"
+    assert red_zip == "alert py-2 alert-danger"
+    # проверка assert - что остальные зелёные
+    fields = [
+        "first-name", "last-name", "address", "e-mail",
+        "phone", "city", "country", "job-position", "company"
+    ]
+    for field_colour in fields:
+        get_field = driver.find_element(By.ID, field_colour).get_attribute("class")
+        green_field = f"{get_field}"
+        assert green_field == "alert py-2 alert-success"
+    driver.quit()
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,70 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
-
-# firefox
-driver = webdriver.Firefox()
-driver.get("https://www.saucedemo.com/")
-
-# авторизация
-input_user = driver.find_element(By.ID, "user-name")
-input_user.send_keys("standard_user")
 
 
-input_pass = driver.find_element(By.ID, "password")
-input_pass.send_keys("secret_sauce")
-
-
-# нажимаем кнопки
-button_login = (driver.find_element(
-    By.CSS_SELECTOR, "#login-button"))
-button_login.click()
-
-sleep(1)
-
-add_backpack = (driver.find_element(
-    By.ID, "add-to-cart-sauce-labs-backpack"))
-add_backpack.click()
-
-
-add_t_shirt = (driver.find_element(
-    By.ID, "add-to-cart-sauce-labs-bolt-t-shirt"))
-add_t_shirt.click()
-
-
-add_onesie = (driver.find_element(
-    By.ID, "add-to-cart-sauce-labs-onesie"))
-add_onesie.click()
-
-
-go_to_chart = (driver.find_element(
-    By.CSS_SELECTOR, ".shopping_cart_container a.shopping_cart_link"))
-go_to_chart.click()
-
-
-checkout = (driver.find_element(By.ID, "checkout"))
-checkout.click()
-
-
-input_first_name = driver.find_element(By.ID, "first-name")
-input_first_name.send_keys("Helen")
-
-
-input_last_name = driver.find_element(By.ID, "last-name")
-input_last_name.send_keys("Mis")
-
-
-postal = driver.find_element(By.ID, "postal-code")
-postal.send_keys("634021")
-
-
-continue_button = (driver.find_element(
-    By.CSS_SELECTOR, ".cart_button.btn_action"))
-continue_button.click()
-
-
-total = driver.find_element(
-    By.CSS_SELECTOR, ".summary_total_label").text
-print(f"{total}")
-
-driver.quit()
+# Вот так вроде надо переоформить в виде одной функции, а не кучи их
+def test_test_03():
+    # firefox
+    driver = webdriver.Firefox()
+    driver.get("https://www.saucedemo.com/")
+    # авторизация
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    # нажимаем кнопки
+    driver.find_element(
+        By.CSS_SELECTOR, "#login-button").click()
+    driver.implicitly_wait(5)
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(
+        By.ID, "add-to-cart-sauce-labs-onesie").click()
+    driver.find_element(
+        By.CSS_SELECTOR, ".shopping_cart_container a.shopping_cart_link").click()
+    driver.find_element(By.ID, "checkout").click()
+    driver.find_element(By.ID, "first-name").send_keys("Helen")
+    driver.find_element(By.ID, "last-name").send_keys("Mis")
+    driver.find_element(By.ID, "postal-code").send_keys("634021")
+    driver.find_element(
+        By.CSS_SELECTOR, ".cart_button.btn_action").click()
+    total = driver.find_element(By.CSS_SELECTOR, ".summary_total_label").text
+    print(f"{total}")
+    driver.quit()
+     # проверка assert
+    total_sum = f"{total}"
+    assert total_sum.strip() == "Total: $58.29"
